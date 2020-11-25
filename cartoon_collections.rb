f033d5ea9431aa9ce00efd32680129152de216a6
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 04:44:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   foods.any? do |food|
-    cheeses.include?(food)
+    cheese_types.include?(food)
     binding.pry
     return food
   end
