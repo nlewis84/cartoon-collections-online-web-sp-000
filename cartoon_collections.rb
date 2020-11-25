a32886b838b41a59fe180944bcdf1fb2b4dafaba
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 04:29:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  array.each do |index|
+    if index.length > 4
+      return true
+    end
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
